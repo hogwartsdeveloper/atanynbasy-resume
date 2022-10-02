c42de2aede8b2e5b1a5185f6101d1e73e5bb880b
zhannur <jannuraidynuly@gmail.com>
2022-10-02 17:13:28 +0600

    refactoring

--- src/message/message/message.controller.ts
+++ src/message/message/message.controller.ts
@@ -9,23 +9,5 @@ export class MessageController {
   @Post()
   setMessage(@Body() messageDto: IMessage) {
     this.messageService.sendTelegramMessage(messageDto);
-    // this.telegramBotService.getUpdates({}).subscribe((d) => {
-    //   const chatId = d[0].message.chat.id;
-    //   if (chatId) {
-    //     this.telegramBotService
-    //       .sendMessage({
-    //         chat_id: chatId,
-    //         text: messageDto.email,
-    //       })
-    //       .subscribe();
-    //
-    //     this.telegramBotService
-    //       .sendMessage({
-    //         chat_id: chatId,
-    //         text: messageDto.message,
-    //       })
-    //       .subscribe();
-    //   }
-    // });
   }
 }
