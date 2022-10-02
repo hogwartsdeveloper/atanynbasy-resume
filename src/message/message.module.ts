900613cbcf7eb09c5f23e88abeaef2fef1ae3273
zhannur <jannuraidynuly@gmail.com>
2022-10-02 16:20:51 +0600

    send message telegram

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MessageController } from './message/message.controller';
+import { TelegramModule } from 'nestjs-telegram';
 
 @Module({
   controllers: [MessageController],
+  imports: [
+    TelegramModule.forRoot({
+      botKey: '5466333407:AAE8cndpOFrzmSjAp7acPhJfQPmMVClrU48',
+    }),
+  ],
 })
 export class MessageModule {}
--- src/message/message/message.controller.ts
+++ src/message/message/message.controller.ts
@@ -1,9 +1,29 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { TelegramService } from 'nestjs-telegram';
 
 @Controller('message')
 export class MessageController {
+  constructor(private telegramBotService: TelegramService) {}
+
   @Post()
   setMessage(@Body() messageDto: { email: string; message: string }) {
-    console.log(messageDto);
+    this.telegramBotService.getUpdates({}).subscribe((d) => {
+      const chatId = d[0].message.chat.id;
+      if (chatId) {
+        this.telegramBotService
+          .sendMessage({
+            chat_id: chatId,
+            text: messageDto.email,
+          })
+          .subscribe();
+
+        this.telegramBotService
+          .sendMessage({
+            chat_id: chatId,
+            text: messageDto.message,
+          })
+          .subscribe();
+      }
+    });
   }
 }
