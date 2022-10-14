1bf81badea57a6fc712e4398281de0bf0b0e52ed
zhannur <jannuraidynuly@gmail.com>
2022-10-14 11:16:00 +0600

    add validators

--- src/message/dto/message.dto.ts
+++ src/message/dto/message.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class MessageDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
 
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty()
   readonly content: string;
 
+  @IsNotEmpty()
+  @MinLength(10)
   @ApiProperty()
   readonly date: string;
 }
--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { MessageController } from './message/message.controller';
 import { TelegramModule } from 'nestjs-telegram';
 import { MessageService } from './message/message.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Message } from '../typeorm';
 
 @Module({
   controllers: [MessageController],
   imports: [
+    TypeOrmModule.forFeature([Message]),
     TelegramModule.forRoot({
       botKey: '5466333407:AAE8cndpOFrzmSjAp7acPhJfQPmMVClrU48',
     }),
