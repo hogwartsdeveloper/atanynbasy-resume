530b90d53416c949099c18bb152a74aae3149cd7
zhannur <jannuraidynuly@gmail.com>
2022-10-15 12:40:50 +0600

    create message gateway

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -4,6 +4,7 @@ import { TelegramModule } from 'nestjs-telegram';
 import { MessageService } from './message/message.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from '../typeorm';
+import { MessageGateway } from './message.gateway';
 
 @Module({
   controllers: [MessageController],
@@ -13,6 +14,6 @@ import { Message } from '../typeorm';
       botKey: '5466333407:AAE8cndpOFrzmSjAp7acPhJfQPmMVClrU48',
     }),
   ],
-  providers: [MessageService],
+  providers: [MessageService, MessageGateway],
 })
 export class MessageModule {}
